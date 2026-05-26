3194a7b27484d957f567cc56e340a9430708ddab
Oleg <olegbyta@gmail.com>
2026-05-27 00:00:00 +0300

    add arrow vercel commit

--- js/script.js
+++ js/script.js
@@ -153,3 +153,44 @@ modalForm.addEventListener(
 
     }
 );
+
+// SCROLL TO TOP BUTTON
+
+const scrollTopBtn =
+    document.getElementById(
+        "scrollTopBtn"
+    );
+
+window.addEventListener(
+    "scroll",
+    () => {
+
+        if(window.scrollY > 500){
+
+            scrollTopBtn.classList.add(
+                "show"
+            );
+
+        }else{
+
+            scrollTopBtn.classList.remove(
+                "show"
+            );
+        }
+
+    }
+);
+
+scrollTopBtn.addEventListener(
+    "click",
+    () => {
+
+        window.scrollTo({
+
+            top: 0,
+
+            behavior: "smooth"
+        });
+
+    }
+);
